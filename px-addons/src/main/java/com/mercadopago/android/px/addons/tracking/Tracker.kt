bf723c59eddf452ccbe337f8fe87a572b27f7682
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2022-01-24 21:45:52 -0300

    Adds GAv2 tracker to enum (#2635)

--- px-addons/src/main/java/com/mercadopago/android/px/addons/tracking/Tracker.kt
+++ px-addons/src/main/java/com/mercadopago/android/px/addons/tracking/Tracker.kt
@@ -3,7 +3,8 @@ package com.mercadopago.android.px.addons.tracking
 enum class Tracker(val bit: Long) {
     MELIDATA(1 shl 0),
     GOOGLE_ANALYTICS(1 shl 1),
-    CUSTOM(1 shl 2);
+    CUSTOM(1 shl 2),
+    GOOGLE_ANALYTICS_V2(1 shl 3);
 
     fun shouldTrack(mask: Long) = mask and bit == bit
 }
